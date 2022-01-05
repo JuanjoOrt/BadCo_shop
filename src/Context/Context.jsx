ac790370d3fedac8f1271474c4925ec2fecdc476
Juanjo Ortiz <jjortiz@ilernaonline.com>
2022-01-05 16:47:49 +0100

    changing default value of user context

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const GlobalContext = React.createContext({})
 
 export function GlobalContextProvider ({ children }) {
-  const [user, setUser] = useState(1)
+  const [user, setUser] = useState(null)
 
   return <GlobalContext.Provider value={{ user, setUser }}>
 		{children}
