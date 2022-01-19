41799e398abc8e7522d96a5cec34ca0a2f3ab883
Juanjo Ortiz <jjortiz@ilernaonline.com>
2022-01-19 10:17:09 +0100

    fxing panel

--- src/Components/LoginPanel/Index.jsx
+++ src/Components/LoginPanel/Index.jsx
@@ -1,6 +1,6 @@
 import './loginPanel.scss'
 import { BiDownArrow, BiUpArrow } from 'react-icons/bi'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLogin } from '../../Hooks/useLogin'
 
 export default function LoginPanel ({ user }) {
@@ -13,13 +13,29 @@ export default function LoginPanel ({ user }) {
         {user}
         {active ? <BiUpArrow className='login-panel-user__icon'/> : <BiDownArrow className='login-panel-user__icon'/>}
       </div>
-      { active &&
-        <div className='login-panel-tab'>
-          <div className='login-panel-tab__item'>Panel de control</div>
-          <div className='login-panel-tab__hr'/>
-          <div className='login-panel-tab__item' onClick={signOut}>Salir</div>
-        </div>
-      }
+      <Panel active={active} signOut={signOut} onClick={() => setActive(false)}/>
     </div>
   )
+}
+
+const Panel = ({ active, signOut, onClick }) => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = () => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClick()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
+  }, [])
+
+  return active &&
+  <div className='login-panel-tab' ref={ref}>
+    <div className='login-panel-tab__item'>Panel de control</div>
+    <div className='login-panel-tab__hr'/>
+    <div className='login-panel-tab__item' onClick={signOut}>Salir</div>
+  </div>
 }
