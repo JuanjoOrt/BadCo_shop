7c27f9abb84ee5f5d208551a21846a1c4271077d
Juanjo Ortiz <jjortiz@ilernaonline.com>
2022-01-31 12:20:43 +0100

    form input fixed

--- src/Components/Forms/InputText/InputText.jsx
+++ src/Components/Forms/InputText/InputText.jsx
@@ -1,18 +1,26 @@
-import { Field } from 'formik'
+import { useField } from 'formik'
+import lang from '../formLang.json'
 import './styles.scss'
 import '../forms.scss'
 
-export default function InputText ({ id, name }) {
+export default function InputText ({ id, ...props }) {
+  const [field, meta] = useField(props)
+  const name = lang[props.name] ? lang[props.name] : props.name
+  const showError = meta.touched && meta.error
+  const classError = showError ? 'input-error' : ''
+
   return (
     <div className='inputText'>
-      <Field
+      <input
         id={id}
-        name={name}
-        className='inputText-input input'
-        placeholder={' '}
+        name={props.name}
+        className={`inputText-input input ${classError}`}
+        placeholder=' '
         type='text'
+        {...field} {...props}
       />
       <label htmlFor={id} className='inputText-label'>{name}</label>
+      { showError && <div className='form-error'>ErrorError</div>}
     </div>
   )
 }
--- src/Pages/Checkout/ProductsHistory/FormCheckout/index.jsx
+++ src/Pages/Checkout/ProductsHistory/FormCheckout/index.jsx
@@ -1,13 +1,16 @@
 import InputText from '../../../../Components/Forms/InputText/InputText'
-import { Formik } from 'formik';
+import { Formik, Form } from 'formik'
 
 export default function FormCheckout () {
   return (
     <div className='form-checkout'>
       <Formik
         initialValues={ { name: '' } }
+        onSubmit={() => console.log('holiwi')}
       >
-      <InputText id='name' name='Nombre'/>
+        <Form>
+          <InputText id='name' name='name'/>
+        </Form>
       </Formik>
     </div>
   )
